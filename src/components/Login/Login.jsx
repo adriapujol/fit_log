bc7a4914f0f4cb549b0d32e379dec75cacbecb9f
Adria Pujol <adriapujolaparicio@gmail.com>
2020-11-25 18:37:59 +0100

    login design ok

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,18 +7,25 @@ function Login() {
         <div className="login">
             <div className="login-content color-overlay">
                 <div className="login-logo">
-                    <img src={barbellLogo} />
+                    <img src={barbellLogo} alt="logo" />
                     <div className="logo-text">
                         <div className="logo-title">GYM LOG</div>
                         <div className="logo-subtitle">Track your lifts</div>
                     </div>
                 </div>
 
-                <form action="">
-                    <input type="text" />
-                    <input type="text" />
-
-                    <button className="btn-login">Login</button>
+                <form className="login-form">
+                    <input
+                        type="password"
+                        placeholder="Username"
+                        className="login-input"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Password"
+                        className="login-input"
+                    />
+                    <button className="login-btn">Login</button>
                 </form>
             </div>
 
